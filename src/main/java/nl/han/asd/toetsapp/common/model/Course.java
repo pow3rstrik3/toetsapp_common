a17ce17366a5aa60190d2ef6a9d8bf6a7bf3346a
Michiel Bos <M.Bos6@student.han.nl>
2017-11-24 15:17:31 +0100

    Updated Course and CourseTest.

--- src/main/java/nl/han/asd/toetsapp/common/model/Course.java
+++ src/main/java/nl/han/asd/toetsapp/common/model/Course.java
@@ -7,22 +7,33 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Course implements JsonModel {
-    //TODO: Add ID
+    private static final String ID = "id";
     private static final String CODE = "code";
     private static final String FULL_NAME = "fullName";
     private static final String EXAM_INFOS = "exams";
+    private int id;
     private String code;
     private String fullName;
     private List<Exam> exams;
 
-    public Course(String code, String fullName) {
+    public Course () {
+
+    }
+
+    public Course(int id, String code, String fullName) {
+        this.id = id;
         this.code = code;
         this.fullName = fullName;
         this.exams = new ArrayList<>();
     }
 
+    /**
+     * Construct a Course from JSON.
+     * @param jsonObject A JSONObject
+     */
     public Course(JSONObject jsonObject) {
-        this(jsonObject.getString(CODE),
+        this(jsonObject.getInt(ID),
+                jsonObject.getString(CODE),
                 jsonObject.getString(FULL_NAME));
         JSONArray examsArray = jsonObject.getJSONArray(EXAM_INFOS);
         for (int i = 0; i < examsArray.length(); i++) {
@@ -33,6 +44,7 @@ public class Course implements JsonModel {
     @Override
     public JSONObject getJSONObject() {
         JSONObject jsonObject = new JSONObject();
+        jsonObject.put(ID, id);
         jsonObject.put(CODE, code);
         jsonObject.put(FULL_NAME, fullName);
 
@@ -43,6 +55,10 @@ public class Course implements JsonModel {
         jsonObject.put(EXAM_INFOS, examArray);
 
         return jsonObject;
+    }
+
+    public int getId() {
+        return id;
     }
 
     public String getCode() {
@@ -63,5 +79,43 @@ public class Course implements JsonModel {
 
     public void removeExam (Exam examInfo) {
         exams.add(examInfo);
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public void setExams(List<Exam> exams) {
+        this.exams = exams;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Course course = (Course) o;
+
+        if (getId() != course.getId()) return false;
+        if (!getCode().equals(course.getCode())) return false;
+        if (!getFullName().equals(course.getFullName())) return false;
+        return getExams().equals(course.getExams());
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getId();
+        result = 31 * result + getCode().hashCode();
+        result = 31 * result + getFullName().hashCode();
+        result = 31 * result + getExams().hashCode();
+        return result;
     }
 }
--- src/test/java/nl/han/asd/toetsapp/common/model/CourseTest.java
+++ src/test/java/nl/han/asd/toetsapp/common/model/CourseTest.java
@@ -8,30 +8,28 @@ public class CourseTest {
 
     @Test
     public void getJSONObjectWithoutExams() throws Exception {
-        final String code = "SWA";
-        final String fullName = "Software Architecture";
-        final Course baseCourse = new Course(code, fullName);
+        final Course baseCourse = new Course(1, "SWA", "Software Architecture");
         final Course jsonCourse = new Course(baseCourse.getJSONObject());
-        assertEquals("Course converted to JSON and back should keep the same code.", code, jsonCourse.getCode());
-        assertEquals("Course converted to JSON and back should keep the same full name.", fullName, jsonCourse.getFullName());
         assertTrue("Course without exam infos converted to JSON and back should have no exam information.", jsonCourse.getExams().isEmpty());
+        assertTrue("Course converted to JSON and back should equal to its base.", jsonCourse.equals(baseCourse));
+        assertEquals("Course converted to JSON and back should have the same hash code.", jsonCourse.hashCode(), baseCourse.hashCode());
+        assertEquals("Course converted to JSON and back should have the same string representation.", jsonCourse.toString(), baseCourse.toString());
     }
 
     @Test
     public void getJSONObjectWithExams() throws Exception {
-        final String code = "APP";
-        final String fullName = "Software Architecture";
         final Exam examInfo1 = new Exam(1, "1.0", "APP Exam 1", ExamType.EXAM, 1485000000, 5400, "Test exam", "You can test this.", false);
         final Exam examInfo2 = new Exam(2, "1.1", "APP Exam 2", ExamType.MOCKEXAM, 1485000000, 5400, "Test exam", "You can test this.", false);
-        final Course baseCourse = new Course(code, fullName);
+        final Course baseCourse = new Course(2, "APP", "Algorithms, Programming Languages and Paradigms");
         baseCourse.addExam(examInfo1);
         baseCourse.addExam(examInfo2);
         final Course jsonCourse = new Course(baseCourse.getJSONObject());
-        assertEquals("Course converted to JSON and back should keep the same code.", code, jsonCourse.getCode());
-        assertEquals("Course converted to JSON and back should keep the same full name.", fullName, jsonCourse.getFullName());
         assertEquals("Course with two exam infos converted to JSON and back should still have two exam infos.", 2, jsonCourse.getExams().size());
         assertTrue("First exam info of copied Course should remain the same.", examInfo1.equals(jsonCourse.getExams().get(0)));
         assertTrue("Second exam info of copied Course should remain the same.", examInfo2.equals(jsonCourse.getExams().get(1)));
+        assertTrue("Course converted to JSON and back should equal to its base.", jsonCourse.equals(baseCourse));
+        assertEquals("Course converted to JSON and back should have the same hash code.", jsonCourse.hashCode(), baseCourse.hashCode());
+        assertEquals("Course converted to JSON and back should have the same string representation.", jsonCourse.toString(), baseCourse.toString());
     }
 
 }
